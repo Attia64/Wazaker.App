4cfafbd0bb2fd14b0c8e7d7ee2708a34b2bb341f
Mohammed Attia <mohammedattiaofficial@gmail.com>
2024-06-06 17:45:34 +0300

    Automation Tasbih Feature

--- app/src/main/java/com/attia/wazaker/ui/fragments/counter/CounterViewModel.kt
+++ app/src/main/java/com/attia/wazaker/ui/fragments/counter/CounterViewModel.kt
@@ -11,6 +11,7 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -20,9 +21,10 @@ class CounterViewModel @Inject constructor(private val historyRepository: Histor
 
     private var checkCount = 0
     var flag = false
-    private var processDuration = 0L
+    private var time = 0
     private var tasbihJob: Job? = null
-
+    private var timingJob: Job? = null
+    private var  lauchCalled = false
     var step = MutableLiveData(1)
 
     private var _counter = MutableLiveData(0)
@@ -31,8 +33,14 @@ class CounterViewModel @Inject constructor(private val historyRepository: Histor
 
     fun increaseCounter() {
         _counter.value = step.value?.let { (_counter.value)?.plus(it) }
-        if (flag) {
+        if (flag && checkCount < 3) {
             checkCount++
+        }
+        if(checkCount == 3 && !lauchCalled) {
+            lauchCalled = true
+            flag = false
+            timingJob?.cancel()
+            launchTasbih()
         }
     }
 
@@ -46,39 +54,40 @@ class CounterViewModel @Inject constructor(private val historyRepository: Histor
         }
     }
 
-    private fun checker(): Boolean {
-        return checkCount == 3
-    }
-
-    private fun setEndTime(): Long {
-        if (checker()) {
-            return System.currentTimeMillis()
-        }
-        return 0L
-    }
-
     fun automationTasbih() {
-        val startTime = System.currentTimeMillis()
         flag = true
-        val endTime = setEndTime()
-        processDuration = ((endTime - startTime) / 1000) / 3
-        Log.i("AutomationTasbih", "StartTime: $startTime & EndTime: $endTime")
-        tasbihJob = automatedTasbihProcess(processDuration)
-        flag = false
-        checkCount = 0
+        lauchCalled  = false
+        timingJob = processDuration()
+        timingJob?.start()
+    }
 
+    private fun launchTasbih() {
+        Log.i("AutomationTasbih", "duration : $time")
+        tasbihJob = automatedTasbihProcess(duration = (time / 3) * 1000L)
+        tasbihJob?.start()
     }
 
     private fun automatedTasbihProcess(duration: Long): Job {
         return viewModelScope.launch {
-            while (true) {
+            while (isActive) {
                 delay(duration)
                 increaseCounter()
+            }
+        }
+    }
+    private fun processDuration(): Job {
+        return viewModelScope.launch {
+            while (flag) {
+                delay(1000)
+                time++
             }
         }
     }
 
     fun cancelAutomationTasbih() {
         tasbihJob?.cancel()
+        checkCount = 0
+        time = 0
+        lauchCalled = false
     }
 }
